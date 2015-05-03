73f94683b533c8b1db8ed914c13d647c0c2c1073
Matthijs <matthijs.booman@hotmail.com>
2015-05-03 18:24:14 +0200

    oops

--- src/main/java/matthbo/plugin/ccooldown/Refs.java
+++ src/main/java/matthbo/plugin/ccooldown/Refs.java
@@ -3,7 +3,7 @@ package matthbo.plugin.ccooldown;
 public class Refs {
 
     public static final String NAME = "CCooldown";
-    public static final String VERSION = "1.1-snapshot-1";
+    public static final String VERSION = "1.2-snapshot-1";
 
     public static final String pluginMSG = "[CCooldown] ";
 
